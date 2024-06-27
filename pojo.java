60bc676968170f1a3b1f94381493bd86d8509edd
GirishBhunatar917 <159538724+GirishBhunatar917@users.noreply.github.com>
2024-06-27 14:12:10 +0530

    Add files via upload

---
+++ pojo.java
@@ -0,0 +1,45 @@
+
+public class pojo {
+
+	
+		private int id;
+		private String name;
+		private String address;
+		private long phone_no;
+		private int pincode;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public long getPhone_no() {
+			return phone_no;
+		}
+		public void setPhone_no(long phone_no) {
+			this.phone_no = phone_no;
+		}
+		public int getPincode() {
+			return pincode;
+		}
+		public void setPincode(int pincode) {
+			this.pincode = pincode;
+		}
+		
+		
+		
+}
+
+
